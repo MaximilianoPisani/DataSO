25aaf75aab414490ddd7ca5708bacba9497b6f5b
MaximilianoPisani <pisanimaximiliano1@gmail.com>
2026-05-29 18:42:08 -0300

    Commit Grid prefab and delay

--- ProyectJamDataSO/Assets/Scripts/Grid/GridScroller.cs
+++ ProyectJamDataSO/Assets/Scripts/Grid/GridScroller.cs
@@ -5,8 +5,32 @@ public class GridScroller : MonoBehaviour
     [Header("Movement")]
     [SerializeField] private float _scrollSpeed = 2f;
 
+    [Header("Start Delay")]
+    [SerializeField] private float _startDelay = 3f;
+
+    private float _timer;
+    private bool _canScroll;
+
     private void Update()
     {
+        HandleDelay();
+
+        if (!_canScroll)
+            return;
+
         transform.position += Vector3.down * _scrollSpeed * Time.deltaTime;
+    }
+
+    private void HandleDelay()
+    {
+        if (_canScroll)
+            return;
+
+        _timer += Time.deltaTime;
+
+        if (_timer >= _startDelay)
+        {
+            _canScroll = true;
+        }
     }
 }
